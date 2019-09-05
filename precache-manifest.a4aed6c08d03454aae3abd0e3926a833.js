34f8f58b31d532a95bcae60e2d44a69e8c6318bd
Alfarel Zaki <alfarell.zaki@gmail.com>
2019-09-05 19:29:03 +0700

    Updates

---
+++ precache-manifest.a4aed6c08d03454aae3abd0e3926a833.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d81a318b04e9184df64598d906850e39",
+    "url": "/react-app1/index.html"
+  },
+  {
+    "revision": "6a592e3815d79eacd5a4",
+    "url": "/react-app1/static/css/main.9151d181.chunk.css"
+  },
+  {
+    "revision": "6bd410afaff528b7f862",
+    "url": "/react-app1/static/js/2.4ffc686c.chunk.js"
+  },
+  {
+    "revision": "6a592e3815d79eacd5a4",
+    "url": "/react-app1/static/js/main.b93c6a3b.chunk.js"
+  },
+  {
+    "revision": "21fbe4f5f8ab191af4c5",
+    "url": "/react-app1/static/js/runtime~main.0b537ca4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
